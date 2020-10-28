fdc09ed8706f10a30efed0eb75737397660ef8c0
Ayush Subedi <ayush.subedi@gmail.com>
2020-10-28 17:54:10 +0545

    resolve #11

--- pydeck_poc/helper.py
+++ pydeck_poc/helper.py
@@ -3,7 +3,7 @@ import pydeck as pdk
 from pydeck_poc import application
 
 
-def deck_example():
+def deck_example1():
     UK_ACCIDENTS_DATA = 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv'
     layer = pdk.Layer(
         'HexagonLayer',  # `type` positional argument is here
@@ -27,5 +27,39 @@ def deck_example():
         bearing=-27.36)
 
     # Combined all of it and render a viewport
-    r = pdk.Deck(layers=[layer], initial_view_state=view_state, mapbox_key=application.config['MAPBOX_AUTH'])
+    r = pdk.Deck(layers=[layer], 
+        initial_view_state=view_state, 
+        mapbox_key=application.config['MAPBOX_AUTH'],
+
+        )
+    return r.to_html(filename=None, as_string=True)
+
+
+def deck_example2():
+    UK_ACCIDENTS_DATA = 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv'
+    layer = pdk.Layer(
+        'ScatterplotLayer',     # Change the `type` positional argument here
+        UK_ACCIDENTS_DATA,
+        get_position=['lng', 'lat'],
+        auto_highlight=True,
+        get_radius=1000,          # Radius is given in meters
+        get_fill_color=[180, 0, 200, 140],  # Set an RGBA value for fill
+        pickable=True)
+
+    # Set the viewport location
+    view_state = pdk.ViewState(
+        longitude=-1.415,
+        latitude=52.2323,
+        zoom=6,
+        min_zoom=5,
+        max_zoom=15,
+        pitch=40.5,
+        bearing=-27.36)
+
+    # Combined all of it and render a viewport
+    r = pdk.Deck(layers=[layer], 
+        initial_view_state=view_state, 
+        mapbox_key=application.config['MAPBOX_AUTH'],
+
+        )
     return r.to_html(filename=None, as_string=True)
--- pydeck_poc/routes.py
+++ pydeck_poc/routes.py
@@ -2,8 +2,13 @@
 from flask import render_template
 from pydeck_poc import application
 from pydeck_poc import basic_auth
-from pydeck_poc.helper import deck_example
+from pydeck_poc.helper import deck_example1, deck_example2
+import random
+
 
 @application.route('/')
 def index():
-    return deck_example()
+    random_plot = [deck_example1(), deck_example2()]
+    return render_template('poc.html',
+                            first = random.choice(random_plot))
+
